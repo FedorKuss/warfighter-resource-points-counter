1e035a5c7001535a876908654131a8f5d610e061
Fedor Kušš (CS) <fedor.kuss@gendigital.com>
2025-11-24 10:08:51 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+console.log('[boot] main.js loading')
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles/main.less' // global styles
 import { zeroClear } from './directives/zeroClear'
 
 const app = createApp(App)
-app.directive('zero-clear', zeroClear)
+
+try {
+  app.directive('zero-clear', zeroClear)
+  console.log('[boot] zero-clear directive registered')
+} catch (e) {
+  console.error('[boot] failed registering directive:', e)
+}
+
 app.mount('#app')
+console.log('[boot] app mounted')
